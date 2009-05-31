e277de4e1b612ac65f165f7b22346c23a631b41d
Douglas Campos <douglas@theros.info>
2009-05-30 23:44:52 -0300

    Added book model

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -1,11 +1,8 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  should_require_attributes :title, :author
-  should_require_unique_attributes :name
-
-  should_have_one :profile
-  should_have_many :dogs
-  should_have_many :messes, :through => :dogs
-  should_belong_to :lover
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
 end
