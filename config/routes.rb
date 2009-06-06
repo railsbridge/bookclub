eaf7ce4db74cf36ecfd80794f5fe7a14fd89e936
Douglas Campos <douglas@theros.info>
2009-06-06 00:18:26 -0300

    basic scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :books
+
   map.resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/migrate/20090606030931_create_books.rb
@@ -0,0 +1,19 @@
+class CreateBooks < ActiveRecord::Migration
+  def self.up
+    create_table :books do |t|
+      t.references :user
+      t.string :title
+      t.string :author
+      t.integer :published_year
+      t.string :genre
+      t.string :isbn
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
